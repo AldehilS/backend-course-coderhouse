826345ef0c11e956b848b2de09dccb3fc0b48c51
Aldehil Sánchez <sanheraldehil@outlook.com>
2024-10-22 17:17:04 -0600

    Add addProduct functionality to cartsController.js and CartManager.js

--- src/controllers/cartsController.js
+++ src/controllers/cartsController.js
@@ -37,3 +37,26 @@ export const createCart = async (req, res) => {
     res.status(500).json({ error: 'Error creating your cart' });
   }
 };
+
+export const addProduct = async (req, res) => {
+  try {
+    const { cartId, productId } = req.params;
+
+    if (!cartId || !productId) {
+      res.status(400).json({ error: "CartId and productId are required" });
+      return;
+    }
+
+    if (isNaN(Number(cartId)) || isNaN(Number(productId))) {
+      res.status(400).json({ error: "CartId and productId must be numbers" });
+      return;
+    }
+
+    const id = await cartManager.addProduct(cartId, productId);
+
+    res.json({ id: id, message: "Product added to cart succesfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error adding product to cart' });
+  }
+}
--- src/models/CartManager.js
+++ src/models/CartManager.js
@@ -14,7 +14,7 @@ export default class CartManager {
       } else {
         carts = JSON.parse(carts);
       }
-      
+
       const id = carts.length === 0 ? 0 : carts[carts.lenght - 1] + 1;
 
       const newCart = { id: id, products: products };
@@ -28,5 +28,41 @@ export default class CartManager {
       console.error(error);
       throw new Error("Error creating cart");
     }
+  }
+
+  async addProduct(cartId, productId) {
+    try {
+      let carts = await fs.promises.readFile(this.path, "utf-8");
+
+      if (carts.length === 0) {
+        carts = [];
+      } else {
+        carts = JSON.parse(carts);
+      }
+
+      const cart = carts.find((cart) => {
+        cart.id === cartId;
+      });
+
+      if (!cart) {
+        throw new Error("Cart not found");
+      }
+
+      const { products } = cart.products;
+
+      // Check if product is already in cart
+      const product = products.find((product) => {
+        product.id === productId;
+      });
+
+      if (product) {
+        product.quantity++;
+      } else {
+        products.push({ id: productId, quantity: 1 });
+      }
+
+      await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 4));
+
+    } catch (error) {}
   }
 }
